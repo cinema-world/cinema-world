2e42c9066a14735de4df1cc6496d1c352297f4e9
Ahmad Ghanem <109623987+15ahmad9@users.noreply.github.com>
2023-10-08 19:45:23 +0300

    Update Footer.js

--- src/Component/Footer/Footer.js
+++ src/Component/Footer/Footer.js
@@ -1,19 +1,1 @@
-import "./Footer.css";
 
-function Footer() {
-    return (
-        <>
-            <div id="footer">
-                <div class="container">
-                    <div class="footer-logo">
-                        <div class="block desc mt-3 mb-4">Cinema World - Watch free movies & TV shows online in HD on any device</div>
-                        <div id="copyright" class="text-center">Copyright &copy;
-                            <script>document.write(new Date().getFullYear())</script> All rights reserved to Cinema World</div>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
-}
-
-export default Footer;
